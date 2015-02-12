1c13501ef9e8819c81bbb19e2724964e7d0b660e
Chyld Medford <chyld.medford@gmail.com>
2015-02-12 15:33:13 -0800

    v1

--- client/main.js
+++ client/main.js
@@ -1,1 +1,23 @@
-console.log('hello, world');
+/* global _ */
+
+'use strict';
+
+$(document).ready(init);
+
+function init() {
+  $('#get-data').click(clickGetData);
+}
+
+function clickGetData() {
+  var year = $('#year').val();
+  var type = $('#type').val();
+  var url = 'https://cdph.data.ca.gov/resource/kbup-p858.json?year=' + year;
+  $.getJSON(url, function(response) {
+
+    var deaths = _.map(response, function(o){return o[type] * 1});
+    deaths = _.compact(deaths);
+    deaths = _.reduce(deaths, function(prev, curr){return prev + curr})
+
+    console.log(deaths);
+  })
+}
